35149327d7c6f2c57a19bcf4e99f4c9be6c3e9ec
Danniel Rolfe <talkto@dannielrolfe.com>
2019-04-14 15:51:12 -0700

    Generate call to action component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CallToActionComponent } from './call-to-action/call-to-action.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CallToActionComponent
   ],
   imports: [
     BrowserModule,
